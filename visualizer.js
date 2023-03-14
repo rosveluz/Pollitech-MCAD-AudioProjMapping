d7d9cb1439663f94bfc0bea6e5537b7a32b75089
rosveluz <112733712+rosveluz@users.noreply.github.com>
2023-03-15 02:28:54 +0800

    randomized particles beta 1

--- visualizer.js
+++ visualizer.js
@@ -4,48 +4,88 @@ function main(){
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    class Bar {
-        constructor(x, y, width, height, color){
-            this.x = x;
-            this.y = y;
-            this.width = width;
-            this.height = height;
-            this.color = color;
-        }
-        update(micInput){
-            this.height = micInput * 2000;
-            this.width = micInput * 500;
-            //this.x++;
-        }
+    class Kaleidoscope {
+        constructor(red, green, blue, index) {
+          this.red = red;
+          this.green = green;
+          this.blue = blue;
+          this.index = index;
+          this.height = 0;
+          this.smoothedMicInput = 0;
+        }       
+        update(micInput) {
+            const smoothingFactor = 0.1;
+            this.smoothedMicInput = smoothingFactor * micInput + (1 - smoothingFactor) * this.smoothedMicInput;
+            this.height = this.smoothedMicInput * 32000;
+            this.width = this.smoothedMicInput * 32000;
+            this.red = this.smoothedMicInput * 255;
+            this.green = Math.random() * 255;
+            this.blue = Math.random() * 255;
+          }
         draw(context){
-            context.fillStyle = this.color;
-            context.fillRect(this.x, this.y, this.width, this.height);
+            context.fillStyle = `rgb(${this.red}, ${this.green}, ${this.blue})`;
+            context.save();
+            context.translate(canvas.width/2, canvas.height/2);
+            context.rotate(this.index * 0.8)
+        
+            for (let i = 0; i < 4; i++) {
+                let angle1 = Math.random() * Math.PI * 2;
+                let angle2 = Math.random() * Math.PI * 2;
+                let x1 = Math.cos(angle1) * this.height;
+                let y1 = Math.sin(angle1) * this.height;
+                let x2 = Math.cos(angle2) * this.height;
+                let y2 = Math.sin(angle2) * this.height;
+                let minRadius = 16; // Minimum radius size
+                let maxRadius = 128; // Maximum radius size
+                let radius1 = Math.random() * (maxRadius - minRadius) + minRadius;
+                let radius2 = Math.random() * (maxRadius - minRadius) + minRadius;
+                context.beginPath();
+                context.arc(x1, y1, radius1, 0, Math.PI * 2);
+                context.fill();
+                context.beginPath();
+                context.arc(x2, y2, radius2, 0, Math.PI * 2);
+                context.fill();
+                context.scale(-1, 1);
+                context.arc(-x2, y2, radius2, 0, Math.PI * 2);
+                context.fill();
+                context.scale(-1, 1);
+            }
+        
+            context.restore();
         }
     }
+    
+    
     const microphone = new Microphone();
-    let bars = [];
-    let barWidth = canvas.width/256;
-    function createBars(){
+    let kaleidoscopes = [];
+    let kaleidoscopeWidth = canvas.width/64;
+    function createKaleidoscopes(){
         for (let i = 0; i < 256; i++){
-            let color = 'hsl(' + i * 2 + ', 100%, 50%)';
-            bars.push(new Bar(i * barWidth, canvas.height/2, 1, 20, color));
+            let color = 'hsl(60%, 10%, 50%)';
+            kaleidoscopes.push(new Kaleidoscope(color, i));
         }
     }
-    createBars();
-    console.log(bars)
+    createKaleidoscopes();
+    console.log(kaleidoscopes)
 
     function animate(){
         if (microphone.initialized){
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            // generates audio samples from microphone
+            ctx.globalAlpha = 0.05;
+            ctx.fillStyle = 'rgba(0, 0, 0, 0.3)';
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            
             const samples = microphone.getSamples();
-            // animate bars based on microphone data
-            bars.forEach(function(bar, i){
-                bar.update(samples[i]);
-                bar.draw(ctx);
+            kaleidoscopes.forEach(function(kaleidoscope, i){
+                setTimeout(function(){
+                    kaleidoscope.update(samples[i]);
+                    kaleidoscope.draw(ctx);
+                }, Math.random()*200);
             });
         }
         requestAnimationFrame(animate);
     }
+    
+    setTimeout(animate, 24);
     animate();
+
 }
